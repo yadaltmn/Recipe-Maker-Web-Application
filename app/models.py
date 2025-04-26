0f3bdf146f2703b81351166de38fa81ec9a0e77a
yadaltmn <jada-lien.nguyen@sjsu.edu>
2025-04-26 03:52:21 +0000

    Update User model to include UserMixin and hashed password support

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from app import db
 from datetime import datetime
+from flask_login import UserMixin
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32))
-    password = db.Column(db.String(32))
+    password = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(32))
 
 class Post(db.Model):
